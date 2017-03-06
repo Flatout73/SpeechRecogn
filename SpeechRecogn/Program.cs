09a98bebb758f8827dbe0cae6465ebc6d0fe3bde
Леонид Лядвейкин <lalyadveykin@edu.hse.ru>
2017-03-07 02:19:28 +0300

    first commit

---
+++ SpeechRecogn/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecogn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ((args.Length < 2) || (string.IsNullOrWhiteSpace(args[0])))
+            {
+                Console.WriteLine("Arg[0]: Specify the endpoint to hit https://speech.platform.bing.com/recognize");
+                Console.WriteLine("Arg[1]: Specify a valid input wav file.");
+
+                return;
+            }
+
+            SpeechCl auth = new SpeechCl("eb76b0ffa0034be39981558ee48641af");
+
+            string requestUri = args[0].Trim(new char[] { '/', '?' });
+            requestUri += @"?scenarios=smd";                                  // websearch is the other main option.
+            requestUri += @"&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5";     // You must use this ID.
+            requestUri += @"&locale=en-US";                                   // We support several other languages.  Refer to README file.
+            requestUri += @"&device.os=Windows 10";
+            requestUri += @"&version=3.0";
+            requestUri += @"&format=json";
+            requestUri += @"&instanceid=565D69FF-E928-4B7E-87DA-9A750B96D9E3";
+            requestUri += @"&requestid=" + Guid.NewGuid().ToString();
+
+            string host = @"speech.platform.bing.com";
+            string contentType = @"audio/wav; codec=""audio/pcm""; samplerate=16000";
+            string audioFile = args[1];
+            string responseString;
+            FileStream fs = null;
+
+            try { 
+
+            var token = auth.GetAccessToken();
+            Console.WriteLine("Token: {0}\n", token);
+            Console.WriteLine("Request Uri: " + requestUri + Environment.NewLine);
+
+            HttpWebRequest request = null;
+            request = (HttpWebRequest)HttpWebRequest.Create(requestUri);
+            request.SendChunked = true;
+            request.Accept = @"application/json;text/xml";
+            request.Method = "POST";
+            request.ProtocolVersion = HttpVersion.Version11;
+            request.Host = host;
+            request.ContentType = contentType;
+            request.Headers["Authorization"] = "Bearer " + token;
+
+                using (fs = new FileStream(audioFile, FileMode.Open, FileAccess.Read))
+                {
+
+                    /*
+                     * Open a request stream and write 1024 byte chunks in the stream one at a time.
+                     */
+                    byte[] buffer = null;
+                    int bytesRead = 0;
+                    using (Stream requestStream = request.GetRequestStream())
+                    {
+                        /*
+                         * Read 1024 raw bytes from the input audio file.
+                         */
+                        buffer = new Byte[checked((uint)Math.Min(1024, (int)fs.Length))];
+                        while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            requestStream.Write(buffer, 0, bytesRead);
+                        }
+
+                        // Flush
+                        requestStream.Flush();
+                    }
+
+                    /*
+                     * Get the response from the service.
+                     */
+                    Console.WriteLine("Response:");
+                    using (WebResponse response = request.GetResponse())
+                    {
+                        Console.WriteLine(((HttpWebResponse)response).StatusCode);
+
+                        using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                        {
+                            responseString = sr.ReadToEnd();
+                        }
+
+                        Console.WriteLine(responseString);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
